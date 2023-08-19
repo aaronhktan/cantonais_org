9fb381ab6ea821c0348c2c9bbf5f5fcd4b2840c4
Aaron Tan <hi@aaronhktan.com>
2023-08-19 01:45:22 -0400

    Set cookies for search term and search type
    - Setting the cookies enables displaying the
      search term that led the user to the entry
      view.
    - Also reformat `urls.py` to use double-quotes
      everywhere.

--- dictionnaire/urls.py
+++ dictionnaire/urls.py
@@ -1,66 +1,87 @@
-from flask import Blueprint
-from .views import *
+from flask import Blueprint, make_response, request, redirect, url_for
+from . import views
 
-dictionary_app = Blueprint('dictionary_app', __name__, static_folder="static",
-                           static_url_path="dictionnaire/static", template_folder="templates")
+
+dictionary_app = Blueprint("dictionary_app", __name__, static_folder="static",
+                           static_url_path="dictionnaire/static",
+                           template_folder="templates")
 
 
 def redirect_post():
     if "search_term" in request.form:
-        search_term = request.form['search_term']
+        search_term = request.form["search_term"]
     else:
-        search_term = request.form['search_term_mobile']
+        search_term = request.form["search_term_mobile"]
     if "search_type" in request.form:
-        search_type = request.form['search_type']
+        search_type = request.form["search_type"]
     else:
-        search_type = request.form['search_type_mobile']
-    return redirect(url_for(f'dictionary_app.{search_type}', search_term=search_term))
+        search_type = request.form["search_type_mobile"]
+
+    return redirect(url_for(f"dictionary_app.{search_type}",
+                            search_term=search_term))
 
 
-@dictionary_app.route("/", methods=('GET', 'POST'))
+@dictionary_app.route("/", methods=("GET", "POST"))
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         return redirect_post()
-    return render_index()
+    return views.render_index()
 
 
-@dictionary_app.route("/recherche/traditionnel/<search_term>", methods=('GET', 'POST'))
+@dictionary_app.route("/recherche/traditionnel/<search_term>",
+                      methods=("GET", "POST"))
 def search_traditional(search_term):
-    if request.method == 'POST':
+    if request.method == "POST":
         return redirect_post()
-    return render_search_traditional(search_term)
+
+    resp = make_response(views.render_search_traditional(search_term))
+
+    resp.set_cookie("search_term", search_term)
+    resp.set_cookie("search_type", search_traditional.__name__)
+
+    return resp
 
 
-@dictionary_app.route("/recherche/simplifie/<search_term>", methods=('GET', 'POST'))
+@dictionary_app.route("/recherche/simplifie/<search_term>",
+                      methods=("GET", "POST"))
 def search_simplified(search_term):
-    if request.method == 'POST':
+    if request.method == "POST":
         return redirect_post()
-    return render_search_simplified(search_term)
+    return views.render_search_simplified(search_term)
 
 
-@dictionary_app.route("/recherche/jyutping/<search_term>", methods=('GET', 'POST'))
+@dictionary_app.route("/recherche/jyutping/<search_term>",
+                      methods=("GET", "POST"))
 def search_jyutping(search_term):
-    if request.method == 'POST':
+    if request.method == "POST":
         return redirect_post()
-    return render_search_jyutping(search_term)
+    return views.render_search_jyutping(search_term)
 
 
-@dictionary_app.route("/recherche/pinyin/<search_term>", methods=('GET', 'POST'))
+@dictionary_app.route("/recherche/pinyin/<search_term>",
+                      methods=("GET", "POST"))
 def search_pinyin(search_term):
-    if request.method == 'POST':
+    if request.method == "POST":
         return redirect_post()
-    return render_search_pinyin(search_term)
+    return views.render_search_pinyin(search_term)
 
 
-@dictionary_app.route("/recherche/fr/<search_term>", methods=('GET', 'POST'))
+@dictionary_app.route("/recherche/fr/<search_term>",
+                      methods=("GET", "POST"))
 def search_french(search_term):
-    if request.method == 'POST':
+    if request.method == "POST":
         return redirect_post()
-    return render_search_french(search_term)
+    return views.render_search_french(search_term)
 
 
-@dictionary_app.route("/entree/<entry>", methods=('GET', 'POST'))
+@dictionary_app.route("/entree/<entry>",
+                      methods=("GET", "POST"))
 def entry_view(entry):
-    if request.method == 'POST':
+    if request.method == "POST":
         return redirect_post()
-    return render_entry(entry)
+
+    search_term = request.cookies.get("search_term")
+    search_type = request.cookies.get("search_type")
+
+    return views.render_entry(entry, search_term=search_term,
+                              search_type=search_type)
